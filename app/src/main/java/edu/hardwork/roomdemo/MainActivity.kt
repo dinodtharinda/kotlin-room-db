68fa8f3d3756079ba98da81ea44d38f29911a1a8
dinod tharinda <dinodtharinda2001@gmail.com>
2024-05-25 21:29:37 +0530

    add data to recycler view

--- app/src/main/java/edu/hardwork/roomdemo/MainActivity.kt
+++ app/src/main/java/edu/hardwork/roomdemo/MainActivity.kt
@@ -1,39 +1,69 @@
 package edu.hardwork.roomdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import edu.hardwork.roomdemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding:ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
         val employeeDao = (application as EmployeeApp).db.employeeDao()
-        binding?.btnAdd?.setOnClickListener{
+        binding?.btnAdd?.setOnClickListener {
             //TODO call addreard with employee
             addRecord(employeeDao)
         }
+        lifecycleScope.launch {
+            employeeDao.fetchAllEmployees().collect{
+                val list = ArrayList(it)
+                setupListOfDataIntoRecyclerView(list,employeeDao)
+            }
+        }
     }
 
-    fun addRecord(employeeDao: EmployeeDao){
+    fun addRecord(employeeDao: EmployeeDao) {
         val name = binding?.etName?.text.toString()
         val email = binding?.etEmailId?.text.toString()
 
-        if(email.isNotEmpty()&&name.isNotEmpty()){
+        if (email.isNotEmpty() && name.isNotEmpty()) {
             lifecycleScope.launch {
-                employeeDao.insert(EmployeeEntity(name=name, email = email))
-                Toast.makeText(this@MainActivity,"record saved",Toast.LENGTH_LONG).show()
+                employeeDao.insert(EmployeeEntity(name = name, email = email))
+                Toast.makeText(this@MainActivity, "record saved", Toast.LENGTH_LONG).show()
                 binding?.etName?.text?.clear()
                 binding?.etEmailId?.text?.clear()
             }
+        } else {
+            Toast.makeText(this@MainActivity, "Name or Email can not be black", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    private fun setupListOfDataIntoRecyclerView(
+        employeesList: ArrayList<EmployeeEntity>,
+        employeeDao: EmployeeDao
+    ) {
+        if (employeesList.isNotEmpty()) {
+            val itemAdapter = ItemAdapter(
+                employeesList
+            )
+            binding?.rvItemsList?.layoutManager = LinearLayoutManager(this)
+            binding?.rvItemsList?.adapter = itemAdapter
+            binding?.rvItemsList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+
+
         }else{
-            Toast.makeText(this@MainActivity,"Name or Email can not be black",Toast.LENGTH_LONG).show()
+            binding?.rvItemsList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
         }
     }
 }
